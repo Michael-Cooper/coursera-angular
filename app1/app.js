d06b1c46ddb7a1e80e697ffd25c2abc4e13ddf11
Michael Cooper <Cooper.MichaelCS@gmail.com>
2017-01-16 11:35:38 -0700

    Add files via upload

---
+++ app1/app.js
@@ -0,0 +1,17 @@
+
+/*start with an immediately invoked function expression (IIFE) */
+(function(){
+    'use strict'; /*blocks accidental declaration of variables with out 'var', which would
+                  allow the variable to be created in the global scope, which we don't want */
+    
+    angular.module("myFirstApp",[])
+    
+            .controller("myFirstController", function($scope){
+        $scope.name = "Mike";
+
+        $scope.sayHello = function(){
+            return "Hello!";
+        };
+    });
+    
+})();
